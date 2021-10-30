8b71e81bf83d71d16a9b0739435d799ab98c21be
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2021-10-29 22:35:51 -0300

    Módulo de Estrutururas

---
+++ MeuApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mouse = new Product(1, "Mouse Logitech", 123.50, ETipoProduto.Produto);
+            var manutencao = new Product(2, "Manutenção Elétrica", 250.00, ETipoProduto.Servico);
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Nome);
+            Console.WriteLine(mouse.Preco);
+            Console.WriteLine((int)mouse.Tipo);
+        }
+    }
+
+    struct Product
+    {
+        public Product(int id, string nome, double preco, ETipoProduto tipo)
+        {
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Tipo = tipo;
+        }
+        public int Id;
+        public string Nome;
+        public double Preco;
+        public ETipoProduto Tipo;
+        public double PrecoEmDolar(double dolar)
+        {
+            return Preco * dolar;
+        }
+    }
+
+    enum ETipoProduto
+    {
+        Produto = 1,
+        Servico = 2
+    }
+}
